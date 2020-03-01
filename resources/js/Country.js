a4d4af30dce5873128b45c21a1cf097b43cd3037
ronpe <ron.peretz1@gmail.com>
2020-03-01 09:17:30 +0200

    Get details from random country button. Delete button now working.

---
+++ resources/js/Country.js
@@ -0,0 +1,16 @@
+export default class Country {
+    constructor(name) {
+        this.name = name;
+    }
+
+    async getCountry() {
+        const data = await fetch(`https://restcountries.eu/rest/v2/name/${this.name}`);
+        const dataJson = await data.json();
+
+        console.log(dataJson[0]);
+        this.region = dataJson[0].region;
+        this.capital = dataJson[0].capital;
+        this.code = dataJson[0].alpha3Code;
+        this.flag = dataJson[0].flag
+    }
+}
---
+++ resources/js/app.js
@@ -0,0 +1,88 @@
+import Country from "./Country.js";
+
+// get all country names for the search bar to autoComplete data and for random country
+const getAllCountriesNames = async () => {
+    const data = await fetch(`https://restcountries.eu/rest/v2/all`);
+    const dataInJson = await data.json();
+    return dataInJson.map(value => value.name);
+};
+
+// update the DOM to insert the new country box
+const renderView = (countryDetails) => {
+    const markup = `
+       <div class="col span-1-of-4">
+            <div class="country_details_box">
+                <img src="${countryDetails.flag}" alt="pic">
+                <ul>
+                    <li><i class="ion-information small-icon"></i><label><strong>Name:</strong> </label>${countryDetails.name.length > 18 ? `${countryDetails.name.slice(0, 18)}...` : countryDetails.name}</li>
+                    <li><i class="ion-code small-icon"></i><label><strong>Code:</strong> </label>${countryDetails.code}</li>
+                    <li><i class="ion-android-globe small-icon"></i><label><strong>Region:</strong> </label>${countryDetails.region}</li>
+                    <li><i class="ion-home small-icon"></i><label><strong>Capital:</strong> </label>${countryDetails.capital}</li>
+                </ul>
+            </div>
+       </div>
+       `;
+
+    selectedCountries.push(markup); // insert to the global array
+    const selectedCountriesFinal =  classifyLastFinalBoxAndFixRow(selectedCountries);
+    document.querySelector('.country-boxes').innerHTML = '';
+    document.querySelector('.country-boxes').innerHTML = selectedCountriesFinal.join('');
+};
+
+const classifyLastFinalBoxAndFixRow = (selectedCountries) => {
+    let currentCountry = '';
+    const selectedCountriesAfterClassify = [];
+    for (let i = 0; i < selectedCountries.length; i++) {
+        if (i === 0 || i % 4 !== 0) {
+            if (i === selectedCountries.length - 1) {
+                currentCountry += selectedCountries[i].replace('country_details_box', 'country_details_box final');
+            } else {
+                currentCountry += selectedCountries[i];
+            }
+        } else {
+            currentCountry = `<div class="row-full">${currentCountry}</div>`;
+            selectedCountriesAfterClassify.push(currentCountry);
+            if (i === selectedCountries.length - 1) {
+                currentCountry = selectedCountries[i].replace('country_details_box', 'country_details_box final');
+            } else {
+                currentCountry = selectedCountries[i];
+            }
+        }
+        if (i === selectedCountries.length - 1) {
+            selectedCountriesAfterClassify.push(`<div class="row-full">${currentCountry}</div>`);
+        }
+    }
+    return selectedCountriesAfterClassify;
+};
+
+
+// get random country - button listener
+document.getElementById('random_country').addEventListener('click', el => {
+    el.preventDefault(); // prevent from page to reload
+    if (countries.length !== 0) {
+        const randomCountry = new Country(countries[Math.floor(Math.random() * countries.length)]);
+        randomCountry.getCountry().then(value => {
+            renderView(randomCountry);
+        })
+    }
+});
+
+// delete all countries from DOM - button listener
+document.getElementById('delete_all_data').addEventListener('click', el => {
+    el.preventDefault(); // prevent from page to reload
+    document.querySelector('.country-boxes').innerHTML = '';
+    selectedCountries = [];
+});
+
+
+// Main //
+let countries = [];
+let selectedCountries = [];
+getAllCountriesNames().then(value => {
+    countries = value;
+}).catch(reason => {
+    document.getElementById('myInput').placeholder = 'Unable load countries';
+});
+
+
+
